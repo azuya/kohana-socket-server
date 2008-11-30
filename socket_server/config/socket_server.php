4ebda5e7982f063948abb7d02ab424a500d1b9de
victor.butler@xilostech.com <victor.butler@xilostech.com@5e54124a-bead-11dd-9405-3d0920048215>
2008-11-30 10:20:17 +0000

    Added some IPC optimizations. Some better destructor handling.

--- socket_server/config/socket_server.php
+++ socket_server/config/socket_server.php
@@ -3,5 +3,5 @@
 $config['enabled'] = TRUE;
 $config['port'] = 10000;
 $config['process_timeout'] = -1; // wait forever
-$config['process_limit'] = 100; // 100 simultaneous clients
+$config['max_clients'] = 100; // 100 simultaneous clients
 $config['read_size'] = 2048; // buffer
